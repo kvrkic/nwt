61f6a282a8792d0ac033698873fed59d4435b1dc
Karlo Vrkic <karlo.vrkic.00@fesb.hr>
2023-07-09 15:11:57 +0200

    Add icon and title

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,27 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
-  return <></>;
+  const [data, setData] = useState('');
+
+  useEffect(() => {
+    axios
+      .get('https://api.chucknorris.io/jokes/random')
+      .then(response => {
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-argument, @typescript-eslint/no-unsafe-member-access
+        setData(response.data.value); // Access the 'value' property containing the joke
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+      });
+  }, []);
+
+  return (
+    <>
+      <div>{data}</div>
+    </>
+  );
 }
 
 export default App;
