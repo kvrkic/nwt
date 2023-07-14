2512000981dfae6e0f2afe2c5667adfb65565201
Karlo Vrkic <karlo.vrkic.00@fesb.hr>
2023-07-14 17:02:41 +0200

    Add form handling

--- client/src/components/Login.tsx
+++ client/src/components/Login.tsx
@@ -1,35 +1,68 @@
-import React from 'react';
+/* eslint-disable @typescript-eslint/no-misused-promises */
+import React, { useState } from 'react';
 import Header from './Header';
+import axios, { AxiosResponse } from 'axios';
+import { LoginResponse } from '../interfaces/login-response.interface';
 
-interface LoginProps {
-  error?: string;
-}
+const Login = () => {
+  const [isVisible, setIsVisible] = useState(true);
+  const [errorMessage, setErrorMessage] = useState('');
 
-const Login: React.FC<LoginProps> = ({ error }) => {
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const formData = new FormData(event.currentTarget);
+
+    const data = {
+      email: formData.get('email') as string,
+      password: formData.get('password') as string,
+    };
+
+    try {
+      const response = await axios.post<AxiosResponse<string | LoginResponse>>(
+        'http://localhost:3000/users/login',
+        data,
+      );
+      console.log(response);
+
+      //handle reponses from server
+      //set cookie to browser and redirect to content
+      //popravit email na backendu da salje na /verify
+
+      console.log(response.data);
+      setIsVisible(false);
+    } catch (error) {
+      console.error(error);
+      setErrorMessage('There was an error');
+    }
+  };
   return (
     <>
       <Header />
-      <div className="container">
-        <h3>Log in:</h3>
-        <form action="/login" method="post">
-          <p>
-            <span>Email:</span>
-            <input type="email" name="email" required />
-          </p>
-          <p>
-            <span>Password:</span>
-            <input
-              type="password"
-              name="password"
-              required
-              minLength={6}
-              maxLength={16}
-            />
-          </p>
-          <input type="submit" />
-        </form>
-        {error && <h1>{error}</h1>}
-      </div>
+      {isVisible && (
+        <div className="container">
+          <h3>Log in:</h3>
+          <form onSubmit={handleSubmit}>
+            <p>
+              <span>Email:</span>
+              <input type="email" name="email" required />
+            </p>
+            <p>
+              <span>Password:</span>
+              <input
+                type="password"
+                name="password"
+                required
+                minLength={6}
+                maxLength={16}
+              />
+            </p>
+            <input type="submit" />
+          </form>
+        </div>
+      )}
+      {!isVisible && <div className="welcome">Login done!</div>}
+      {errorMessage && <div className="welcome">{errorMessage}</div>}
     </>
   );
 };
--- client/src/components/Register.tsx
+++ client/src/components/Register.tsx
@@ -1,45 +1,89 @@
-import React from 'react';
+/* eslint-disable @typescript-eslint/no-misused-promises */
+import React, { useState } from 'react';
 import Header from './Header';
+import axios, { AxiosResponse } from 'axios';
 
-interface LoginProps {
-  error?: string;
-}
+const Register = () => {
+  const [isVisible, setIsVisible] = useState(true);
+  const [errorMessage, setErrorMessage] = useState('');
 
-const Login: React.FC<LoginProps> = ({ error }) => {
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const formData = new FormData(event.currentTarget);
+
+    const data = {
+      firstName: formData.get('firstName') as string,
+      lastName: formData.get('lastName') as string,
+      email: formData.get('email') as string,
+      password: formData.get('password') as string,
+    };
+
+    try {
+      const response = await axios.post<AxiosResponse<string>>(
+        'http://localhost:3000/users/register',
+        data,
+      );
+      console.log(response);
+      // @todo
+      // krenia san pisat ovo
+      // axios post na /users/register, napisat poruku da triba verificirat mail,
+      // napravit novu komponentu i path /verify na koji ce ga vodit link iz maila
+      // poigrat se sa query parametrima, primit token iz maila i napravit axios post sa tokenon
+      // prikazat korisniku poruku da je verificirat uspjesno ili nije, smislit sta onda
+      // resend path napravit slicno ko i verify, koji radi axios post na backend sa mailon u bodyu
+      // i onda prikaze odgovor korisniku
+
+      // if (response.data === 'User created') {
+      //   console.log('tocno tako je i bilo');
+      // }
+      // if(response.data == Object)
+
+      console.log(response.data);
+      setIsVisible(false);
+    } catch (error) {
+      console.error(error);
+      setErrorMessage('There was an error');
+    }
+  };
+
   return (
     <>
       <Header />
-      <div className="container">
-        <h3>Create account:</h3>
-        <form action="/register" method="post">
-          <p>
-            <span>First Name:</span>
-            <input type="text" name="firstName" required />
-          </p>
-          <p>
-            <span>Last Name:</span>
-            <input type="text" name="lastName" required />
-          </p>
-          <p>
-            <span>Email:</span>
-            <input type="email" name="email" required />
-          </p>
-          <p>
-            <span>Password:</span>
-            <input
-              type="password"
-              name="password"
-              required
-              minLength={6}
-              maxLength={16}
-            />
-          </p>
-          <input type="submit" />
-        </form>
-        {error && <h1>{error}</h1>}
-      </div>
+      {isVisible && (
+        <div className="container">
+          <h3>Create account:</h3>
+          <form onSubmit={handleSubmit}>
+            <p>
+              <span>First Name:</span>
+              <input type="text" name="firstName" required />
+            </p>
+            <p>
+              <span>Last Name:</span>
+              <input type="text" name="lastName" required />
+            </p>
+            <p>
+              <span>Email:</span>
+              <input type="email" name="email" required />
+            </p>
+            <p>
+              <span>Password:</span>
+              <input
+                type="password"
+                name="password"
+                required
+                minLength={6}
+                maxLength={16}
+              />
+            </p>
+            <input type="submit" />
+          </form>
+        </div>
+      )}
+      {!isVisible && <div className="welcome">Verification email sent!</div>}
+      {errorMessage && <div className="welcome">{errorMessage}</div>}
     </>
   );
 };
 
-export default Login;
+export default Register;
